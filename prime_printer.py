ed7cc6d175ec3c676646b2909b74c62bcc2e37a3
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-04 09:46:43 +0330

    Update prime_printer.py

--- prime_printer.py
+++ prime_printer.py
@@ -1,3 +1,17 @@
 import numpy as np 
 import prime_number_checker as pnc
-from prime_number_checker import prime
+from prime_number_checker import prime_checker 
+import sys
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+if a >  b : 
+  c = a 
+  a = b 
+  b = c
+l = [] 
+for i in range(a , b , 1 ) : 
+  if prime_checker(i) : 
+    l.append(i)
+print(l)
+
+
