fcd6e423921000329ccd5347e815ea626efc22e6
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-04 09:29:29 +0330

    Update prime_number_checker.py

--- prime_number_checker.py
+++ prime_number_checker.py
@@ -1,1 +1,15 @@
-
+import numpy as np 
+class prime_checker : 
+  def __init__(self):
+    pass 
+  def is_prime(self , x ) : 
+    if x < 2 : 
+      return False
+    elif x ==2 : 
+      return True 
+    else : 
+      for i in range(int(x**0.5) +1 ) :
+        if x%i ==0 :
+          return False
+      return True 
+        
